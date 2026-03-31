42012065b9db8c3bf51fe83e27ed16d036639d9d
GIGAS <bonjin.app@gmail.com>
2026-03-31 10:04:25 +0900

    release: v0.1.58

--- examples/__scaffold_test_global_shortcut__79141/worker/forge_worker/__init__.py
+++
@@ -1,3 +0,0 @@
-from .protocol import write_ready, write_success, write_error, write_progress, read_request, log
-from .dispatcher import register, dispatch, list_actions
-from .runner import run_worker
--- examples/__scaffold_test_minimal__79141/electron/preload.ts
+++
@@ -1,6 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-import type { WorkerRequest } from '@forge/ipc-contract';
-
-contextBridge.exposeInMainWorld('api', {
-  execute: (request: WorkerRequest) => ipcRenderer.invoke('worker:execute', request),
-});
